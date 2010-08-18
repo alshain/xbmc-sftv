9e628f570abc8316c0e60c59b2ecc91ec0ef4099
alshain <c.vonrueti@project-infcom.ch>
2010-08-19 01:03:02 +0200

    Module to parse input from server and such

---
+++ resources/lib/parser.py
@@ -0,0 +1,8 @@
+from resources.lib.util import getJson, getXml
+def parse_video_information(raw):
+    raw = raw.splitlines()[1]
+    return getJson(raw)
+
+def parse_wall(raw):
+    return getXml(raw)
+
